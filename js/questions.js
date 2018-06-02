0b28c27a183a475b85752b9c1f092a180bbc5122
Ryan Achten <ryanachten@gmail.com>
2018-06-02 16:45:00 +1200

    Add init material design styles, Revise existing colors

--- js/questions.js
+++ js/questions.js
@@ -1,7 +1,19 @@
 const questionObj = {
   general : [
-    { question: "What did you learn yesterday/this week?",
-      answer: "meow"
+    { question: "What did you learn yesterday / this week?",
+      answer: "This week I have started learning Backbone.js as a means of furthering my frontend framework knowledge. I will later apply this by refactoring a previous project of mine."
+    },
+    { question: "What excites or interests you about coding?",
+      answer: "I like the fact that it is a field which is constantly evolving allows for endless learning and challenges as well as creative expression."
+    },
+    { question: "What is a recent technical challenge you experienced and how did you solve it?",
+      answer: "My personal site was taking ages to load. To find out what was delaying the load process, I popped open Chrome’s network tool and, as I suspected, it was bloated images taking ages to be retrieved. I then used an online compression tool called optimizer to compress these by around 60%, reducing the load time dramatically."
+    },
+    { question: "When building a new web site or maintaining one, can you explain some techniques you have used to increase performance?",
+      answer: "A useful tool for getting ideas on where to start is the Google Pagespeed Insights tool, which provides a desktop and mobile rating for websites as well as useful suggestions\nIf using Webpack, use a production-ready src maps for production, vs the bloated version for development.\nOnline compression tools such as SVGOMG and Optimizer for jpg and png to reduce load time of my assets.\nAll else fails, employ compression algorithms on the build process or server, such as gzip and/or brotli"
+    },
+    { question: "When building a new web site or maintaining one, can you explain some techniques you have used to increase performance?",
+      answer: "Getting SEO working for SPA’s can be a pain; using Google Webmaster, you can aid your site’s indexability using verification tokens, requests for crawlers\nAlso having clear, human-readable meta descriptions and titles w/o any repeating keywords helps these crawlers understand your site"
     }
   ],
   html : [
